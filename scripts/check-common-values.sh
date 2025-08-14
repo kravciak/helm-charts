be7f016bf9776b7132913f819b06a9696321137c
Martin Kravec <kravciak@gmail.com>
2025-08-14 14:45:56 +0200

    Fix brackets to run code in current shell
    Signed-off-by: Martin Kravec <kravciak@gmail.com>

--- scripts/check-common-values.sh
+++ scripts/check-common-values.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 # Check that there's no divergence between ./common-values.yaml, key `global`,
 # and the `global` key on the helm-chart values.yaml
 
-diff <(yq eval '(.global // {}) | sort_keys(.)' common-values.yaml) <(yq eval '(.global // {}) | sort_keys(.)' charts/kubewarden-controller/values.yaml) || (
+diff <(yq eval '(.global // {}) | sort_keys(.)' common-values.yaml) <(yq eval '(.global // {}) | sort_keys(.)' charts/kubewarden-controller/values.yaml) || {
 	echo
 	echo "kubewarden-controller values.yaml diverges from common-values.yaml"
 	exit 1
-)
-diff <(yq eval '(.global // {}) | sort_keys(.)' common-values.yaml) <(yq eval '(.global // {}) | sort_keys(.)' charts/kubewarden-defaults/values.yaml) || (
+}
+diff <(yq eval '(.global // {}) | sort_keys(.)' common-values.yaml) <(yq eval '(.global // {}) | sort_keys(.)' charts/kubewarden-defaults/values.yaml) || {
 	echo
 	echo "kubewarden-defaults values.yaml diverges from common-values.yaml"
 	exit 1
-)
+}
